a19bc6ab0b93f57011bfa1997ef0c677363a934e
jonah <falsefriend13@gmail.com>
2017-12-29 15:37:55 -0500

    i give up for now

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var app = angular.module('musicApp', []);
 
-angular.module('tabsDemoDynamicHeight', ['ngMaterial']);
-
 app.controller('musicCtrl', function($scope, $http) {
   $http.get("../mconn.php").then(function (response) {
     $scope.Data = response.data;
