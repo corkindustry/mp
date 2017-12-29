f967c82e80bebe9f86fd9e1ce98c76122d7a6aa0
jonah <falsefriend13@gmail.com>
2017-12-29 10:57:48 -0500

    1st step
    move to mysql

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = angular.module('musicApp', []);
 
 app.controller('musicCtrl', function($scope, $http) {
-  $http.get("../music_json/2017music.json").then(function (response) {
+  $http.get("../mconn.php").then(function (response) {
     $scope.Data2017 = response.data;
   });
-  $http.get("../music_json/2016music.json").then(function (response) {
+  /*$http.get("../music_json/2016music.json").then(function (response) {
     $scope.Data2016 = response.data;
   });
   $http.get("../music_json/2015music.json").then(function (response) {
@@ -12,5 +12,5 @@ app.controller('musicCtrl', function($scope, $http) {
   });
   $http.get("../music_json/2014music.json").then(function (response) {
     $scope.Data2014 = response.data;
-  });
+  });*/
 });
